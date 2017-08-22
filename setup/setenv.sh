264eb4576877494cd03aac075f77ae3ec243f926
mat2uken <ku@yasagure.jp>
2017-08-22 12:30:58 +0900

    added setenv.sh

---
+++ setup/setenv.sh
@@ -0,0 +1,6 @@
+PWD=`pwd`/
+export ARCH=arm; export CROSS_COMPILE=arm-linux-
+export PATH=${PWD}buildroot-2017.05.2/output/host/usr/bin:$PATH
+export INSTALL_MOD_PATH=${PWD}buildroot-2017.05.2/output/target
+export BULDROOT_PATH=${PWD}buildroot-2017.05.2
+
